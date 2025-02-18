dc953ef9f2299c0808d54faf2fdf6933fde4df49
Devendra Ingle <101554961+DevendraIngle@users.noreply.github.com>
2025-02-18 22:39:22 +0530

    Update ai_agent.py

--- ai_agent.py
+++ ai_agent.py
@@ -1,25 +1,19 @@
-# if you dont use pipenv uncomment the following:
 from dotenv import load_dotenv
 load_dotenv()
 
-#Step1: Setup API Keys for Groq, OpenAI and Tavily
 import os
 
 GROQ_API_KEY=os.environ.get("GROQ_API_KEY")
 TAVILY_API_KEY=os.environ.get("TAVILY_API_KEY")
 #OPENAI_API_KEY=os.environ.get("OPENAI_API_KEY")
 
-#Step2: Setup LLM & Tools
 from langchain_groq import ChatGroq
-#from langchain_openai import ChatOpenAI
 from langchain_community.tools.tavily_search import TavilySearchResults
 
-#openai_llm=ChatOpenAI(model="gpt-4o-mini")
 groq_llm=ChatGroq(model="deepseek-r1-distill-llama-70b")
 
 search_tool=TavilySearchResults(max_results=2)
 
-#Step3: Setup AI Agent with Search tool functionality
 from langgraph.prebuilt import create_react_agent
 from langchain_core.messages.ai import AIMessage
 
